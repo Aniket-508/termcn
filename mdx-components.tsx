ad56d46665b0bd9a3db517200b73d8342dcc74e5
Aniket-508 <pawaraniket508@gmail.com>
2026-04-08 09:43:54 +0530

    chore: lint fixes

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -21,7 +21,6 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ThemeShowcasePreview } from "@/examples/theme-showcase";
 import { cn } from "@/lib/utils";
 
 export const mdxComponents = {
